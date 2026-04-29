9ab3e47ef5d3c433aae73ae7be893ddddaf11e10
aakhan10 <aakhan@mail.csuchico.edu>
2026-04-29 11:52:34 -0700

    Unit Testing

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -15,8 +15,96 @@ class GuesserTest : public ::testing::Test
 };
 
 // Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+TEST(GuesserTest, DistanceIsZeroForExactMatch)
 {
   Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+  EXPECT_EQ(object.distance("Secret"), 0);
+}
+
+TEST(GuesserTest, DistanceCountsDifferentCharactersSameLength)
+{
+  Guesser object("Secret");
+  EXPECT_EQ(object.distance("SecRet"), 1);
+  EXPECT_EQ(object.distance("Sxxxxx"), 5);
+}
+
+TEST(GuesserTest, DistanceCountsExtraCharactersInLongerGuess)
+{
+  Guesser object("Secret");
+  EXPECT_EQ(object.distance("Secret123"), 3);
+}
+
+TEST(GuesserTest, DistanceCountsMissingCharactersInShorterGuess)
+{
+  Guesser object("Secret");
+  EXPECT_EQ(object.distance("Sec"), 3);
+}
+
+TEST(GuesserTest, DistanceCannotExceedSecretLength)
+{
+  Guesser object("Secret");
+  EXPECT_EQ(object.distance("CompletelyDifferentAndVeryLong"), 6);
+}
+
+TEST(GuesserTest, DistanceWorksWithEmptyGuess)
+{
+  Guesser object("Secret");
+  EXPECT_EQ(object.distance(""), 6);
+}
+
+// match() tests
+
+TEST(GuesserTest, MatchReturnsTrueForCorrectGuess)
+{
+  Guesser object("Secret");
+  EXPECT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, MatchReturnsFalseForIncorrectGuess)
+{
+  Guesser object("Secret");
+  EXPECT_FALSE(object.match("SecRet"));
+}
+
+TEST(GuesserTest, CorrectGuessResetsRemainingGuesses)
+{
+  Guesser object("Secret");
+
+  EXPECT_FALSE(object.match("Secrex"));
+  EXPECT_FALSE(object.match("Secrey"));
+
+  EXPECT_TRUE(object.match("Secret"));
+
+  EXPECT_FALSE(object.match("Secrex"));
+  EXPECT_FALSE(object.match("Secrey"));
+  EXPECT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, ThreeConsecutiveWrongGuessesLockObject)
+{
+  Guesser object("Secret");
+
+  EXPECT_FALSE(object.match("Secrex"));
+  EXPECT_FALSE(object.match("Secrey"));
+  EXPECT_FALSE(object.match("Secrez"));
+
+  EXPECT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, BruteForceGuessLocksObject)
+{
+  Guesser object("Secret");
+
+  EXPECT_FALSE(object.match("Wrong"));
+  EXPECT_FALSE(object.match("Secret"));
+}
+
+
+TEST(GuesserTest, LockedObjectAlwaysReturnsFalse)
+{
+  Guesser object("Secret");
+
+  EXPECT_FALSE(object.match("TotallyWrong"));
+  EXPECT_FALSE(object.match("Secret"));
+  EXPECT_FALSE(object.match("Secret"));
 }
